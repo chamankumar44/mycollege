1155b40f000f15d25bd7ad97b6bda5b5d0e4ca58
Ayush <ayushvrma07@gmail.com>
2025-08-26 08:35:41 +0530

    Testing

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,41 @@ public class Main {
         rahul.Developer();
         rahul.lawyer();
 
+//        MyHome obj = new MyHome();
+//        String feedback =  obj.watchTv ("nice");
+//        System.out.println("My Feedback is. " + feedback);
+
+        Swimming swimg= new Swimming();
+      String x= swimg.swimg("Ayush");
+        System.out.println("The Swimming pool is " +x  );
+
+    }
+}
+
+
+
+
+class MyHome {
+    public String watchTv (String feedback){
+        //Watching TV
+        return feedback + "  Very Good ";
+    }
+
+}
+
+
+class Swimming {
+
+    public String swimg (String feedback){
+        //swimming
+        return feedback +" nice" ;
+    }
+}
+
+class Gym {
+    public int doWorkout (int set){
+        //upper body and lowerbody
+        return set;
     }
 }
 
